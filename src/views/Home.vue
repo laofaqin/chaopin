0450cc424fb5f7c2ddbb746e5f1b92cdc75dc95b
MoonCup <47781583+moon-cup@users.noreply.github.com>
2019-08-21 21:36:03 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,44 @@
 <template>
   <div>
-    <h2>Home</h2>
-    <button @click="tap()">anniu</button>
+    <van-nav-bar @click-right="goSearch()" @click-left='goCart()'  left-arrow class="nav-bar">
+      <van-icon class="cart-icon" name="cart-circle-o" slot="left" color='white'/>
+      <van-search disabled class="search" placeholder="搜索您想要的商品" slot="right" />
+    </van-nav-bar>
   </div>
 </template>
 
 <script>
+
+
 export default {
-  data() {
-    return {
-      list: []
-    };
-  },
-  methods: {
-    tap() {
-      
-      this.$axios({
-        url: "http://jx.xuzhixiang.top/ap/api/productlist.php",
-        params: { uid: 19802 }
-      }).then(data => {
-        console.log(data.data);
-      });
+  data(){
+    return{
+      value:''
     }
   },
-  mounted() {
-    this.$axios({
-      url: "http://jx.xuzhixiang.top/ap/api/productlist.php",
-      params: { uid: 19802 }
-    }).then(data => {
-      console.log(data);
-    });
+  methods:{
+    goSearch(){
+      this.$router.push('/Search');
+      console.log(111)
+    },
+    goCart(){
+      this.$router.push('/Cart')
+    }
   }
-};
+}
 </script>
 
-<style scoped>
+<style>
+  .nav-bar{
+    height:60px;
+    background:rgba(0, 0, 0, 0.8)
+  }
+  .search{
+    padding:0;
+    margin: 9px 17px;
+    width: 280px;
+  }
+  .cart-icon{
+    font-size: 24px;
+  }
 </style>
