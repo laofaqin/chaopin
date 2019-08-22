e35c6a55718c71c82f02a7c3bf303857864187d5
MoonCup <47781583+moon-cup@users.noreply.github.com>
2019-08-22 22:40:59 +0800

    Create SportShoes.vue

---
+++ src/views/SportShoes.vue
@@ -0,0 +1,33 @@
+<template>
+  <van-nav-bar class="sportshoes"
+  title="运动鞋"
+  left-arrow
+  @click-left="onClickLeft"
+/>
+</template>
+
+<script>
+export default {
+    methods:{
+        onClickLeft() {
+            console.log('fanhui')
+            this.$router.go(-1)
+        }
+    }
+}
+</script>
+
+<style>
+    .van-nav-bar{
+        height:60px;
+        line-height: 60px;
+        background:rgba(0, 0, 0, 0.8);
+        
+    }
+    .van-nav-bar__title{
+        color:white;
+    }
+    .van-nav-bar .van-icon{
+        color:white;
+    }
+</style>
