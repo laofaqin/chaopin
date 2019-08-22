0d3299c4c2064417b3720c482100000d60ace8b5
winner <465550479@qq.com>
2019-08-22 22:22:43 +0800

    Create MyEstimate.vue

---
+++ src/views/MyEstimate.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <header>
+      <div class="wddd">
+        <van-icon name="arrow-left" class="arrow" v-tap="{methods:goMine}" />
+        <p>我的评价</p>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+     name:'MyEstimate',
+  data(){
+    return{
+      
+    }
+  },
+  methods: {
+    goMine(){
+      this.$router.push('/Mine')
+    }
+  },
+
+};
+</script>
+
+<style scoped>
+.wddd {
+  height: 40px;
+  background: black;
+  display: flex;
+}
+.wddd p {
+  color: whitesmoke;
+  margin-top: 10px;
+  margin-left: 120px;
+}
+.arrow {
+  color: whitesmoke;
+  margin-left: 12px;
+  margin-top: 12px;
+}
+</style>
