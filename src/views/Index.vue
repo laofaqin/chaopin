f99dc83dee5e4dc36aeaae2eae71907f146565bb
Lingzq <980654477@qq.com>
2019-08-23 21:37:23 +0800

    Update Index.vue

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -7,8 +7,8 @@
 		<van-tabbar v-model="active">
 		  <van-tabbar-item icon="wap-home" to="/Home">首页</van-tabbar-item>
 		  <van-tabbar-item icon="apps-o" to="/ProSort">分类</van-tabbar-item>
-		  <van-tabbar-item icon="shopping-cart-o" to="/Cart">购物车</van-tabbar-item>
-		  <van-tabbar-item icon="user-o" to="/Mine">我的</van-tabbar-item>
+		  <van-tabbar-item icon="shopping-cart-o" @click='cart()'>购物车</van-tabbar-item>
+		  <van-tabbar-item icon="user-o" @click='setting()'>我的</van-tabbar-item>
 		</van-tabbar>		
 	</div>
 </template>
@@ -24,6 +24,20 @@
 		methods:{
 			getTitle(msg){
 				this.title=msg
+			},
+			cart(){
+				if(localStorage.getItem("userInfo")){
+					this.$router.push('/Cart');					
+				}else{
+					this.$router.push('/login');					
+				}
+			},
+			setting(){
+				if(localStorage.getItem("userInfo")){
+					this.$router.push('/Mine');					
+				}else{
+					this.$router.push('/login');
+				}
 			}
 		}
 	}
