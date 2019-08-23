a9f29941bdd43d6b587f9327928b0b315dd03ba1
winner <465550479@qq.com>
2019-08-23 15:19:13 +0800

    Update Index.vue

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -5,10 +5,10 @@
 		</div>
 		
 		<van-tabbar v-model="active">
-		  <van-tabbar-item icon="home-o" to="/Home">首页</van-tabbar-item>
-		  <van-tabbar-item icon="search" to="/ProSort">分类</van-tabbar-item>
-		  <van-tabbar-item icon="friends-o" to="/Cart">购物车</van-tabbar-item>
-		  <van-tabbar-item icon="setting-o" to="/Mine">我的</van-tabbar-item>
+		  <van-tabbar-item icon="wap-home" to="/Home">首页</van-tabbar-item>
+		  <van-tabbar-item icon="apps-o" to="/ProSort">分类</van-tabbar-item>
+		  <van-tabbar-item icon="shopping-cart-o" to="/Cart">购物车</van-tabbar-item>
+		  <van-tabbar-item icon="user-o" to="/Mine">我的</van-tabbar-item>
 		</van-tabbar>		
 	</div>
 </template>
