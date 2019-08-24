c71625f8b455ee0699008276641d0efc84d6dc1a
winner <465550479@qq.com>
2019-08-24 20:55:56 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ import Index from '@/views/Index'
 
 Vue.use(Router)
 
-export default new Router({
+const router =  new Router({
   routes: [
     {
       path: '/',
@@ -22,35 +22,53 @@ export default new Router({
         {
           path: '/Home',
           name: 'Home',
-          component: () => import('../views/Home')
+          component: () => import('../views/Home'),
+          meta:{
+            title:'首页'
+          }
         },
         {
           path: '/ProSort',
           name: 'ProSort',
           redirect:'/ShoeSort',
           component: () => import('../views/ProSort'),
+          meta:{
+            title:'产品分类'
+          },
           children:[
           	{
           		path:'/shoesort',
           		name:'shoesort',         		
- 						component:()=>import('../views/ShoeSort')
+               component:()=>import('../views/ShoeSort'),
+               meta:{
+                title:'运动鞋'
+              },
           	},
           	{
           		path:'/bagsort',
           		name:'bagsort',
-          		component:()=>import('../views/BagSort')
+              component:()=>import('../views/BagSort'),
+              meta:{
+                title:'箱包'
+              },
           	}
           ]
         },
         {
           path: '/Cart',
           name: 'Cart',
-          component: () => import('../views/Cart')
+          component: () => import('../views/Cart'),
+          meta:{
+            title:'购物车'
+          }
         },
         {
           path: '/Mine',
           name: 'Mine',
-          component: () => import('../views/Mine')
+          component: () => import('../views/Mine'),
+          meta:{
+            title:'我的'
+          }
         }
       ]
     },
@@ -67,7 +85,10 @@ export default new Router({
     {
       path:'/Search',
       name:'Search',
-      component:()=>import('../views/Search')
+      component:()=>import('../views/Search'),
+      meta:{
+        title:'搜索'
+      }
     },
     // {
     //   path:'/Cart',
@@ -77,17 +98,26 @@ export default new Router({
     {
     	path: '/login',
       name: 'login',
-      component:() => import('../views/Login')
+      component:() => import('../views/Login'),
+      meta:{
+        title:'登陆'
+      }
     },
     {
     	path: '/register',
       name: 'register',
-      component:() => import('../views/Register')
+      component:() => import('../views/Register'),
+      meta:{
+        title:'注册'
+      }
     },
     {
       path:'/SportShoes',
       name:'SportShoes',
-      component:()=>import('../views/SportShoes')
+      component:()=>import('../views/SportShoes'),
+      meta:{
+        title:'运动鞋'
+      }
     },
     //这一块注释是 覃永发 临时放的,后期有用, 不要删.
     // {
@@ -113,82 +143,130 @@ export default new Router({
     {
       path:'/Setting',
       name:'Setting',
-      component:() => import('../views/Setting')
+      component:() => import('../views/Setting'),
+      meta:{
+        title:'设置'
+      }
     },
     {
       path:'/MyOrder',
       name:'MyOrder',
-      component:() => import('../views/MyOrder') 
+      component:() => import('../views/MyOrder'),
+      meta:{
+        title:'我的订单'
+      } 
     },
     {
       path:'/AllOrder',
       name:'AllOrder',
-      component:() => import('../views/AllOrder')
+      component:() => import('../views/AllOrder'),
+      meta:{
+        title:'我的订单'
+      }
     },
     {
       path:'/Sent',
       name:'Sent',
-      component:() => import('../views/Sent')
+      component:() => import('../views/Sent'),
+      meta:{
+        title:'我的订单'
+      }
     },
     {
       path:'/GetPro',
       name:'GetPro',
-      component:() => import('../views/GetPro')
+      component:() => import('../views/GetPro'),
+      meta:{
+        title:'我的订单'
+      }
     },
     {
       path:'/MyPrice',
       name:'MyPrice',
-      component:() => import('../views/MyPrice')
+      component:() => import('../views/MyPrice'),
+      meta:{
+        title:'我的砍价'
+      }
     },
     {
       path:'/MyCollect',
       name:'MyCollect',
-      component:() => import('../views/MyCollect')
+      component:() => import('../views/MyCollect'),
+      meta:{
+        title:'我的收藏'
+      }
     },
     {
       path:'/MyEstimate',
       name:'MyEstimate',
-      component:() => import('../views/MyEstimate')
+      component:() => import('../views/MyEstimate'),
+      meta:{
+        title:'我的评价'
+      }
     },
     {
       path:'/MyAddress',
       name:'MyAddress',
-      component:() => import('../views/MyAddress')
+      component:() => import('../views/MyAddress'),
+      meta:{
+        title:'我的地址'
+      }
     },
     {
       path:'/NewAddress',
       name:'NewAddress',
-      component:() => import('../views/NewAddress')
+      component:() => import('../views/NewAddress'),
+      meta:{
+        title:'新增地址'
+      }
     },
     {
       path:'/MyCoupon',
       name:'MyCoupon',
-      component:() => import('../views/MyCoupon')
+      component:() => import('../views/MyCoupon'),
+      meta:{
+        title:'优惠券'
+      }
     },
     {
       path:'/Service',
       name:'Service',
-      component:() => import('../views/Service')
+      component:() => import('../views/Service'),
+      meta:{
+        title:'联系客服'
+      }
     },
     {
       path:'/MyInfo',
       name:'MyInfo',
-      component:() => import('../views/MyInfo')
+      component:() => import('../views/MyInfo'),
+      meta:{
+        title:'个人信息'
+      }
     },
     {
       path:'/About',
       name:'About',
-      component:() => import('../views/About')
+      component:() => import('../views/About'),
+      meta:{
+        title:'关于我们'
+      }
     },
     {
       path:'/Suggestion',
       name:'Suggestion',
-      component:() => import('../views/Suggestion')
+      component:() => import('../views/Suggestion'),
+      meta:{
+        title:'投诉建议'
+      }
     },
     {
     	path: '/getword',
       name: 'getword',
-      component:() => import('../views/GetWord')
+      component:() => import('../views/GetWord'),
+      meta:{
+        title:'找回密码'
+      }
     },
     {
     	path: '/GetCut',
@@ -207,3 +285,14 @@ export default new Router({
     },
   ]
 })
+
+
+
+router.beforeEach((to,from,next)=>{
+  if(to.meta.title){
+    document.title = to.meta.title
+  }
+  next()
+})
+
+export default router;
