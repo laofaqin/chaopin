9f5edbb5b98562d6da4c0183595309653dfb7d88
MoonCup <47781583+moon-cup@users.noreply.github.com>
2019-08-26 22:39:59 +0800

    Update mutations.js

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,8 +1,37 @@
-import router from '../router'
-
 var mutations={
 	change(state){
         state.show = !state.show
-    }
+    },
+    nike(state){
+        state.title = '耐克'
+    },
+    conver(state){
+        state.title = '匡威'
+    },
+    adidas(state){
+        state.title = '阿迪达斯'
+    },
+    vans(state){
+        state.title = 'vans'
+    },
+    sport(state){
+        state.proSortName = '运动鞋'
+    },
+    bag(state){
+        state.proSortName = '箱包'
+    },
+    coach(state){
+        state.title = '蔻驰'
+    },
+    mcm(state){
+        state.title = 'MCM'
+    },
+    maike(state){
+        state.title = '迈克.科尔斯'
+    },
+    furla(state){
+        state.title = '芙拉'
+    },
+    
 }
 export default mutations;
