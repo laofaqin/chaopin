954173cf025a22a2ce37fc8a547d8170e97ca124
winner <465550479@qq.com>
2019-08-26 22:03:59 +0800

    Update MyCollect.vue

--- src/views/MyCollect.vue
+++ src/views/MyCollect.vue
@@ -4,31 +4,44 @@
       <div class="wddd">
         <van-icon name="arrow-left" class="arrow" v-tap="{methods:goMine}" />
         <p>我的收藏</p>
+        <van-icon name="delete" class="delete" v-tap='{methods:Delete}'/>
       </div>
     </header>
     <section>
       <van-card
-        num="0"
-        price="2.00"
-        desc="描述信息"
-        title="商品标题"
-        thumb="https://img.yzcdn.cn/vant/t-thirt.jpg"
+        :price="detail.price"
+        :desc="detail.descriptions"
+        :title="detail.name"
+        :thumb="detail.coverImg"
       />
     </section>
   </div>
 </template>
 
 <script>
+import api from '../api/api_try'
 export default {
   name: "MyCollect",
   data() {
-    return {};
+    return {
+      detail:''
+    };
   },
   methods: {
     goMine() {
       this.$router.push("/Mine");
+    },
+    Delete(){
+      this.detail = ''
     }
-  }
+  },
+  mounted() {
+    let pid = '5d612315b91b9569e466e9ab'
+    api.getDetail(pid).then(res=>{
+      // console.log(res)
+      this.detail = res.data
+    })
+  },
 };
 </script>
 
@@ -41,11 +54,17 @@ export default {
 .wddd p {
   color: whitesmoke;
   margin-top: 10px;
-  margin-left: 120px;
+  margin-left: 135px;
 }
 .arrow {
   color: whitesmoke;
   margin-left: 12px;
   margin-top: 12px;
+}
+.delete{
+  font-size: 14px;
+  color: whitesmoke;
+  margin-left:135px;
+  margin-top:12px;
 }
 </style>
