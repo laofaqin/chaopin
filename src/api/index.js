5ef1b3ff9a7cb14cf102ea2879251af916cb454d
MoonCup <47781583+moon-cup@users.noreply.github.com>
2019-08-27 19:10:20 +0800

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -7,7 +7,6 @@ let base=process.env.API_ROOT;
 axios.interceptors.request.use(function (config) {
     // 在发送请求之前做些什么
     if(localStorage.getItem('token')){
-		console.log('请求了token')
     		config.headers.common['authorization'] = "Bearer "+localStorage.getItem('token');
     }
     return config;
@@ -20,7 +19,6 @@ axios.interceptors.request.use(function (config) {
 // 添加响应拦截器
 axios.interceptors.response.use(function (response) {
     // 对响应数据做点什么
-    console.log('获取到数据了')
     return response;
   }, function (error) {
     // 对响应错误做点什么
