349597bc6907c9ece9cb957e5e1c6350e83cae42
winner <465550479@qq.com>
2019-08-29 14:55:45 +0800

    Update MyOrder.vue

--- src/views/MyOrder.vue
+++ src/views/MyOrder.vue
@@ -2,12 +2,12 @@
   <div>
     <header>
       <div class="wddd">
-        <van-icon name="arrow-left" class="arrow" v-tap='{methods:goMine}'/>
+        <van-icon name="arrow-left" class="arrow" v-tap="{methods:goMine}" />
         <p>我的订单</p>
       </div>
     </header>
     <section>
-      <van-tabs  v-model="active">
+      <van-tabs v-model="active">
         <van-tab title="全部"></van-tab>
         <van-tab title="未付款"></van-tab>
         <van-tab title="代发货"></van-tab>
@@ -19,48 +19,54 @@
         <span>2019/08/28 08:50</span>
         <p>等待买家付款</p>
       </div>
-        <van-card
-          v-for="(item,i) in orders" :key="i"
-          num="1"
-          :price="item.price"
-          desc="描述信息"  
-          title="商品标题"
-          thumb="https://img.yzcdn.cn/vant/t-thirt.jpg"
-        />
+
+      <van-card
+      v-for='(item,i) in orders'
+      :key='i'
+        num="1"
+        :price="item.price"
+        desc="描述信息"
+        title="商品标题"
+        thumb="https://img.yzcdn.cn/vant/t-thirt.jpg"
+      >
+        
+        <div slot="footer">
+          <p>订单编号:{{item.no}}</p>
+          <p>共1件商品 总计: ￥{{item.price}}</p>          
+        </div>
+      </van-card>
+
       
-      <div class="price">
-        <p>共1件商品 总计: ￥ 999.00</p>
-      </div>
     </section>
   </div>
 </template>
 
 <script>
-import api from '../api/api_zhang'
+import api from "../api/api_zhang";
 export default {
-  name:'MyOrder',
-  data(){
-    return{
-      active:0,
-      orders:[]
-    }
+  name: "MyOrder",
+  data() {
+    return {
+      active: 0,
+      orders: []
+    };
   },
   methods: {
-    goMine(){
-      this.$router.push('/Mine')
+    goMine() {
+      this.$router.push("/Mine");
     }
   },
   mounted() {
-      let params = {
-        per:10,
-        page:1
-      }
-      api.getOrders(params).then(res=>{
-        //console.log(res.data.orders)
-        this.orders = res.data.orders
-        // console.log(this.orders)
-      })
-  },
+    let params = {
+      per: 10,
+      page: 1
+    };
+    api.getOrders(params).then(res => {
+      //console.log(res.data.orders)
+      this.orders = res.data.orders;
+      console.log(this.orders);
+    });
+  }
 };
 </script>
 
@@ -80,28 +86,28 @@ export default {
   margin-left: 12px;
   margin-top: 12px;
 }
-.times{
+.times {
   height: 40px;
   border-bottom: 1px solid #cecece;
   display: flex;
 }
-.times span{
-  margin-top:10px;
-  margin-left:10px;
-  font-size:14px;
+.times span {
+  margin-top: 10px;
+  margin-left: 10px;
+  font-size: 14px;
 }
-.times p{
+.times p {
   margin-left: 155px;
   font-size: 14px;
   margin-top: 10px;
 }
-.price{
-  height:40px;
+.price {
+  height: 40px;
   border-top: 1px solid #cecece;
   border-bottom: 1px solid #cecece;
 }
-.price p{
+.price p {
   font-size: 14px;
-  margin-left:200px;
+  margin-left: 200px;
 }
 </style>
